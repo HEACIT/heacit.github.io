2dd4cb69a2aebf0f2c1d7874b862e0d5b65c9d3e
Jelle de Plaa <j.de.plaa@sron.nl>
2024-10-07 15:17:19 +0200

    Change the copyright line to CC-BY

--- source/conf.py
+++ source/conf.py
@@ -60,3 +60,5 @@ html_static_path = ['_static']
 
 # Add the HTML base URL for sphinx-sitemap
 html_baseurl = 'https://heacit.github.io/'
+
+copyright = u'2024, HEACIT, Creative Commons Attribution 4.0 (CC-BY)'
